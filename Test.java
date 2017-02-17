790f0052eed94e06136d14f1a6980e69a74b0aa5
harshitsinha9000 <harshitsinha9000@gmail.com>
2017-02-17 15:05:00 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,18 @@
+//assignment 3.1
+package java_session3;
+
+public class Test {
+
+		
+			static{
+			print(10);
+			}
+			static void print (int x) {
+			System.out.println (x);
+			System.exit(0);
+			}
+	}
+
+
+//output =  Main method not found in class HelloWorld, please define the main method as:                                                                                                        
+//			public static void main(String[] args)                                      
